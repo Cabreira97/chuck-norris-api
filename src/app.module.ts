817f3e742756ca0cf1b8b4a78748257762b10546
Cabreira97 <thiagovieiramachado97@gmail.com>
2024-07-02 18:47:46 -0300

    ✨  Add ConfigModule and update imports in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { HttpModule } from '@nestjs/axios';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config'; // Adicionando ConfigModule
+import { JokesModule } from './chuck-norris/chuck-norris.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule,
+    JokesModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
